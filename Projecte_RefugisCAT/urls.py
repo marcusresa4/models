b328fc423ab696ab345a8c197a56755dcdf02392
Marc Resa Enrich <marcresaenrich@MacBook-MacBook-Pro-de-Marc.local>
2021-03-17 23:29:11 +0100

    simple canvi al url admin

--- Projecte_RefugisCAT/urls.py
+++ Projecte_RefugisCAT/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from apps.RefugisCAT import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('administrarcomptes/', admin.site.urls),
     path('', views.welcome),
     path('register/', views.register),
     path('login/', views.login),
