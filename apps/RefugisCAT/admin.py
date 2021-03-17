f177cafafe4db340a32c97d4db67336fbe092ab4
Marc Resa Enrich <marcresaenrich@MacBook-MacBook-Pro-de-Marc.local>
2021-03-17 19:05:00 +0100

    models fets i interficie admin permet crear objevctes

--- apps/RefugisCAT/admin.py
+++ apps/RefugisCAT/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from apps.RefugisCAT.models import Refugi, Ressenya, Servei
 
-# Register your models here.
+
+class RefugiAdmin(admin.ModelAdmin):
+    pass
+
+
+class ServeiAdmin(admin.ModelAdmin):
+    pass
+
+
+class RessenyaAdmin(admin.ModelAdmin):
+    pass
+
+
+# Defined empty classes PostAdmin and CategoryAdmin. For the purposes of this tutorial,
+# you do not need to add any attributes or methods to these classes. They are used
+# to customize what is shown on the admin pages.
+
+admin.site.register(Refugi, RefugiAdmin)
+admin.site.register(Servei, ServeiAdmin)
+admin.site.register(Ressenya, RessenyaAdmin)
+
+# These last two lines are the most important. These register the models with the
+# admin classes.
--- apps/RefugisCAT/models.py
+++ apps/RefugisCAT/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Refugi(models.Model):
+    nom_Refugi = models.CharField(max_length=70)
+    coordenades = models.CharField(max_length=100)
+    descripcio_Refugi = models.TextField()
+    valoracio_Refugi = models.FloatField()
+    serveis = models.ManyToManyField('Servei', related_name='serveis', blank=True)
+
+    # preview_image = models.FilePathField(path="/img")
+    # reference_link = models.URLField(max_length=250)
+
+    def __str__(self):
+        return self.nom_Refugi
+
+
+class Servei(models.Model):
+    nom_Servei = models.CharField(max_length=70)
+    descripcio_Servei = models.TextField()
+    preu_Servei = models.FloatField()
+
+    def __str__(self):
+        return self.nom_Servei
+
+class Ressenya(models.Model):
+    nom_Autor = models.CharField(max_length=70)
+    text_Ressenya = models.TextField()
+    valoracio = models.IntegerField()
+    data_Creacio = models.DateTimeField(auto_now_add=True)
+    refugi = models.ForeignKey('Refugi', on_delete=models.CASCADE)
+
+# CharField is used for short strings and specifies a maximum length.
+# TextField is similar to CharField but can be used for longer form text as it
+# doesn’t have a maximum length limit.
+# FilePathField also holds a string but must point to a file path name.
+# URLField is a CharField for an URL, validated by URLValidator.
+
+# __str__ function returns the representation, as string, for every Project instance.
