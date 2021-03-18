54e5ceade1a4dc7d858700782ed310c1972a6637
root <root@LAPTOP-K8D8G0S9.localdomain>
2021-03-18 18:50:47 +0100

    Search in main page

--- apps/RefugisCAT/views.py
+++ apps/RefugisCAT/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as do_login
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
 
 def welcome(request):
     #Si estàs identificat, vas a la pantalla d'inici
@@ -19,9 +22,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     form = UserCreationForm()
+
+    form.fields['username'].help_text = None
+    form.fields['password1'].help_text = None
+    form.fields['password2'].help_text = None
+
     if request.method == "POST":
         # S'afegeixen les dades que introdueix l'usuari
         form = UserCreationForm(data=request.POST)
+        
         # Si les dades son valides
         if form.is_valid():
 
@@ -54,6 +63,11 @@ def login(request):
             if user is not None:
                 do_login(request, user)
                 return redirect('/')
+    else:	
+        if request.GET.get("Entra sense registrar-se")=="Entra sense registrar-se":
+            return render(request, "plantilles/welcome.html", {'form': form})
+        else:
+            return render(request, "plantilles/login.html", {'form': form})
 
     return render(request, "plantilles/login.html", {'form': form})
 
